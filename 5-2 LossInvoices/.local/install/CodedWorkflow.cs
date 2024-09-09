7b383077b97adc945a79cf5c16aa7a6a8569849a
HsingKuangLai <R11722016@ntu.edu.tw>
2024-09-09 21:20:09 +0800

    labeling

---
+++ 5-2 LossInvoices/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Exercise5
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
---
+++ 6-2 InvoiceExtraction/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Exercise6
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
